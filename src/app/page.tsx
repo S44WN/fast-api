42e2a72bdc4f0ecd573931a3f7e686b20a5711fe
Saawn <itissaavn@gmail.com>
2024-05-11 19:37:30 +0530

    Add: input, input-value with state, make requests for input text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,39 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [input, setInput] = useState<string>(""); // input value
+  const [searchResults, setSearchResults] = useState<{
+    results: string[];
+    duration: number;
+  }>(); // search results
+
+  // useEffect hook to run when input changes
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!input) {
+        return setSearchResults(undefined);
+      }
+
+      const response = await fetch(`/api/search?query=${input}`);
+    };
+
+    fetchData();
+  }, [input]);
+
   return (
-    <>
-      <h1>Hey world</h1>
-    </>
+    <div>
+      {/* input has value which triggers handler function on onChange */}
+      <input
+        type="text"
+        value={input}
+        onChange={(event) => {
+          setInput(event.target.value);
+        }}
+      ></input>
+    </div>
   );
 }
